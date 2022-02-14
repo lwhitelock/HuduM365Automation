fac64f19308c0c37a038e25fdf5d9a7d0c299fa6
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2022-02-14 21:06:26 +0000

    Update run.ps1

--- Get-M365Tenants/run.ps1
+++ Get-M365Tenants/run.ps1
@@ -2,7 +2,7 @@ param($name)
 $customerExclude = $env:customerExclude -split ','
 $Authheaders = Get-GraphToken -tenantid $env:Tenantid
 [System.Collections.Generic.List[PSCustomObject]]$Customers = (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/contracts?`$top=999" -tenantid $script:Tenantid -Headers $Authheaders) | Select-Object CustomerID, DefaultdomainName, DisplayName | Where-Object -Property DisplayName -NotIn $customerExclude
-if ([bool]$env:DocumentPartnerTenant -eq $true) {
+if ([System.Convert]::ToBoolean($env:DocumentPartnerTenant) -eq $true) {
     $null = $Customers.add([PSCustomObject]@{
         customerId        = $env:TenantID
         defaultDomainName = $env:PartnerDefaultDomain
