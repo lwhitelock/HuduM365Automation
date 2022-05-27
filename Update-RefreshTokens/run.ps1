dfb59e9ed750765805f99af7f4203fe3fa927fd9
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2022-05-27 11:34:01 +0100

    Version 1.2 Fixed Refresh Token

--- Update-RefreshTokens/run.ps1
+++ Update-RefreshTokens/run.ps1
@@ -1,26 +1,28 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
+Write-Host "Starting Token Refresh"
 # Get the current universal time in the default string format.
 $currentUTCtime = (Get-Date).ToUniversalTime()
 
 $Refreshtoken = (Get-GraphToken -ReturnRefresh $true).Refresh_token
 $ExchangeRefreshtoken = (Get-GraphToken -AppID 'a0c73c16-a7e3-4564-9a95-2bdf47383716' -refreshtoken $ENV:ExchangeRefreshtoken -ReturnRefresh $true).Refresh_token
 
-$ResourceGroup = $ENV:Website_Resource_Group
-$Subscription = ($ENV:WEBSITE_OWNER_NAME).split('+') | Select-Object -First 1
 if ($env:MSI_SECRET) {
     Disable-AzContextAutosave -Scope Process | Out-Null
-    $AzSession = Connect-AzAccount -Identity -Subscription $Subscription
+    Write-Host "Connecting to Azure AD"
+    $AzSession = Connect-AzAccount -Identity
 }
-$KV = Get-AzKeyVault -SubscriptionID $Subscription -ResourceGroupName $ResourceGroup
 
-if ($Refreshtoken) { 
-    Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $Refreshtoken -AsPlainText -Force)
+Write-Host "Getting Key Vault"
+$KV = $ENV:WEBSITE_DEPLOYMENT_ID
+
+if ($Refreshtoken -and $KV) { 
+    Set-AzKeyVaultSecret -VaultName $kv -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $Refreshtoken -AsPlainText -Force)
 }
 else { Write-Host "Could not update refresh token. Will try again in 7 days."}
-if ($ExchangeRefreshtoken) {
-    Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'ExchangeRefreshToken' -SecretValue (ConvertTo-SecureString -String $ExchangeRefreshtoken -AsPlainText -Force)
+if ($ExchangeRefreshtoken -and $KV) {
+    Set-AzKeyVaultSecret -VaultName $kv -Name 'ExchangeRefreshToken' -SecretValue (ConvertTo-SecureString -String $ExchangeRefreshtoken -AsPlainText -Force)
     Write-Host "System API: Updated Exchange Refresh token."
 }
 else {
